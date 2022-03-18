90b57edb4eb459355a520d1b68b51e62762069de
Harrison Bowlby <hrbowlby@gmail.com>
2022-03-18 11:05:46 -0700

    added username and password to user model

--- models/user.js
+++ models/user.js
@@ -22,6 +22,16 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    username:
+    {
+        type: String,
+        required: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    }
 
 })
 
