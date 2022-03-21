f0b2032b225b31e085cad806cd2820012f278fa2
Brandon Short <brandonjshort@gmail.com>
2022-03-21 13:35:09 -0600

    stuff

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,11 +6,6 @@ const router = express.Router();
 
 router.post('/post',reviewsController.createPost);  
 router.put('/post/:reviewId', reviewsController.updatePost);
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 7f28c3b43a6639a7c11cad5db5857741d7f8d7ef
 
 
 
