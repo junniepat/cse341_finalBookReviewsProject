a7a0ba89afdd2c60785c7665220c87171e8b8781
Darcee Quebe <darceerl@byui.edu>
2022-03-23 21:27:47 -0600

    Add dotenv for loading secrets

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// installed express, nodemon, body parser, 
+// installed express, nodemon, body parser,
 const express = require("express");
 const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
@@ -13,11 +13,9 @@ const reviewModel = require('./models/review');
 const app = express();
 
 //stores mongodb connection for later use
-const MONGODB_URI =
+const MONGODB_URI = process.env.MONGODB_URL;
 
  // replace with your token;
-
-
 
 app.use(bodyParser.json());
 
@@ -26,7 +24,7 @@ var options = {
     explorer: true,
     customCss: '.swagger-ui .topbar { display: none }'
   };
-  
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 // prevents CORS error
