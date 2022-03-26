1deb1e1d8794e5509fd4eb5eb5afd67118bc2953
Brandon Short <71357028+bshort95@users.noreply.github.com>
2022-03-25 20:56:18 -0600

    Update auth.js
    oops. really implemented validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,78 @@
-const express = require('express');
-const reviewsController = require('../controllers/reviews');
-const { body } = require('express-validator');
-
-const router = express.Router();
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-router.post('/post',
-    [
-    body('rating')
-      .isNumeric()
-      .isInt({min: 0, max:5}),
-    body('reviewSummary')
-      .trim()
-      .isLength({ min: 5 })  
-  ],
-reviewsController.createReview);
+const User = require('../models/user');
 
 
-router.put('/post/:reviewId',[
-    body('rating')
-      .isNumeric()
-      .isInt({min: 0, max:5}),
-    body('reviewSummary')
-      .trim()
-      .isLength({ min: 5 })  
-  ], reviewsController.updateReview);
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({message: errors});
+  }
+  const email = req.body.email;
+  const username = req.body.username;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        username: username,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: hashedPw,
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
-router.get('/reviews', reviewsController.getReviews);
-router.get('/review/:id', reviewsController.getReview);
-router.delete('/review/:id', reviewsController.deleteReview);
-
-module.exports = router;
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        process.env.SECRET,
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
